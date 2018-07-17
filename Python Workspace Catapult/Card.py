652cbd0e6fb107b36d133770613b83f63ef7d39c
GenoBeno <41342635+GenoBeno@users.noreply.github.com>
2018-07-17 14:13:29 -0400

    Add files via upload
    Max Chen upload

---
+++ Python Workspace Catapult/Card.py
@@ -0,0 +1,33 @@
+import pygame
+import time
+import os
+
+pygame.init()
+
+class Card():
+    def __init__(self, value, suit, imageName):
+        self.cardValue = value
+        self.suitValue = suit
+        self.imageAddress = imageName
+    
+    def getCardValue(self):
+        return self.cardValue
+
+    def getSuitValue(self):
+        return self.suitValue
+
+    def getImageAddress():
+        return self.imageAddress
+
+    def displayCardImage(self, location, screen):
+        image = pygame.image.load(os.path.join(self.imageAddress))
+        image = pygame.transform.scale(image.convert_alpha(), (100, 100))
+        screen.blit(image, location)
+        pygame.display.flip()
+
+
+    
+
+
+
+
